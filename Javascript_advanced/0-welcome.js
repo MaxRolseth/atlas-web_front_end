930a57f4b84aff63a59ebd495d935ea7f1ecfeed
Max Rolseth <max.rolseth@atlasschool.com>
2024-04-30 09:40:42 -0500

    Added JS task 0

---
+++ Javascript_advanced/0-welcome.js
@@ -0,0 +1,15 @@
+/* Task 0 */
+
+function welcome(firstName, lastName) {
+  let fullName = firstName + " " + lastName;
+
+  function displayFullName() {
+      alert("Welcome" + " " + fullName);
+  }
+
+  displayFullName();
+}
+
+/* RUN WITH */
+welcome('Holberton', 'School');
+alert(fullName);
